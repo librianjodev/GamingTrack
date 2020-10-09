b9a275895208bf5e251c49a2566313da2bec316c
pedrojonassm <pedrojonassm@gmail.com>
2020-10-09 19:21:47 -0300

    Apaguei tudo, tentar refazer d e novo kk

--- source/GamimgTrack/views.py
+++ source/GamimgTrack/views.py
@@ -1,15 +1,3 @@
-from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from django.shortcuts import render
 
-
-def register(response):
-    if response.method == "POST":
-	form = RegisterForm(response.POST)
-	if form.is_valid():
-	    form.save()
-
-	return redirect("/home")
-    else:
-	form = RegisterForm()
-
-    return render(response, "register/register.html", {"form":form})
+# Create your views here.
