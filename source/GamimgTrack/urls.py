d25a6ce8a8fabb790a97e6bd986a2cad6dc786da
pedrojonassm <pedrojonassm@gmail.com>
2020-10-23 19:02:26 -0300

    Movido todas as views de usuario para Views_user

--- source/GamimgTrack/urls.py
+++ source/GamimgTrack/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from GamimgTrack import views
+from GamimgTrack import views_user
 
 urlpatterns = [
-    path('register/', views.RegisterUser), 
-    path('', views.LoginUser), 
-    path('changeMyPass/', views.AlterarSenhaUser), 
-    path('changeMyName/', views.AlterarLoginUser), 
-    path('changeMyEmail/', views.AlterarNomeUser), 
-    path('deleteMyUser/', views.DeletarMinhaConta), 
-    path('listarUsers/', views.ListarUsuarios),
-    path('apagarConta/', views.ApagarOutraConta),
+    path('register/', views_user.RegisterUser), 
+    path('', views_user.LoginUser), 
+    path('changeMyPass/', views_user.AlterarSenhaUser), 
+    path('changeMyName/', views_user.AlterarLoginUser), 
+    path('changeMyEmail/', views_user.AlterarNomeUser), 
+    path('deleteMyUser/', views_user.DeletarMinhaConta), 
+    path('listarUsers/', views_user.ListarUsuarios),
+    path('apagarConta/', views_user.ApagarOutraConta),
 ]
