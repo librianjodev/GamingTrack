392cccaa2646d2b0c174af8c593baf369e85ab8b
pedrojonassm <pedrojonassm@gmail.com>
2020-10-23 10:48:00 -0300

    Um usuario moderador agora consegue apagar outra conta

--- source/GamimgTrack/urls.py
+++ source/GamimgTrack/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from GamimgTrack import views
 
 urlpatterns = [
-    path('register/', views.registerUser), 
-    path('', views.loginUser), 
-    path('changePass/', views.alterarSenhaUser), 
-    path('changeName/', views.alterarLoginUser), 
-    path('changeEmail/', views.alterarNomeUser), 
-    path('deleteUser/', views.deletarUser), 
-    path('listarUsers/', views.ListarUsuarios), 
+    path('register/', views.RegisterUser), 
+    path('', views.LoginUser), 
+    path('changeMyPass/', views.AlterarSenhaUser), 
+    path('changeMyName/', views.AlterarLoginUser), 
+    path('changeMyEmail/', views.AlterarNomeUser), 
+    path('deleteMyUser/', views.DeletarMinhaConta), 
+    path('listarUsers/', views.ListarUsuarios),
+    path('apagarConta/', views.ApagarOutraConta),
 ]
--- source/GamimgTrack/views.py
+++ source/GamimgTrack/views.py
@@ -13,7 +13,7 @@ IrParaListarUsers = "user/lista_users.html"
 IrParaVisita = "user/visitar_outro.html"
 MensagemErro = "Algo de errado não está certo"
 
-def registerUser(response):
+def RegisterUser(response):
     if response.method == "POST":
         form = RegisterUserForm(response.POST)
 
@@ -36,7 +36,7 @@ def registerUser(response):
     else:
         form = RegisterUserForm()
         return render(response, IrParaRegistrar, {"form":form})
-def loginUser(response):
+def LoginUser(response):
     
     if response.method == "POST":
         form = LoginUserForm(response.POST)
@@ -49,14 +49,14 @@ def loginUser(response):
             if User.objects.filter(login=lo, password=p).count() == 0:
                 return JsonResponse(data = {"message": "Login ou senha incorretos"})
             else:
-                sla = User.objects.get(login=lo, password=p)
-                response.session['id_user'] = sla.id
-                return render(response, IrParaInfo, {"user":sla})
+                logado = User.objects.get(login=lo, password=p)
+                response.session['id_user'] = logado.id
+                return render(response, IrParaInfo, {"user":logado})
     else:
         form = LoginUserForm()
         return render(response, IrParaLogin, {"form":form})
-def alterarSenhaUser(response):
-    sla = User.objects.get(id = response.session['id_user'])
+def AlterarSenhaUser(response):
+    logado = User.objects.get(id = response.session['id_user'])
     if response.method == "POST":
         form = ChangeUserPasswordForm(response.POST)
 
@@ -64,62 +64,62 @@ def alterarSenhaUser(response):
             p = form.cleaned_data["password"]
             p1 = form.cleaned_data["password1"]
             p2 = form.cleaned_data["password2"]
-            if (sla.password == p2) and (p == p1):
-                sla.password = p
-                sla.save()
-                return render(response, IrParaInfo, {"user":sla})
+            if (logado.password == p2) and (p == p1):
+                logado.password = p
+                logado.save()
+                return render(response, IrParaInfo, {"user":logado})
             else:
                 return JsonResponse(data = {"message": MensagemErro})
     else:
         form = ChangeUserPasswordForm()
         return render(response, IrParaAlterar, {"form":form})
 
-def alterarNomeUser(response):
-    sla = User.objects.get(id = response.session['id_user'])
+def AlterarNomeUser(response):
+    logado = User.objects.get(id = response.session['id_user'])
     if response.method == "POST":
         form = ChangeUserNameForm(response.POST)
 
         if form.is_valid():
             n = form.cleaned_data['nome']
             p = form.cleaned_data['password']
-            if (sla.password == p):
-                sla.nome = n
-                sla.save()
-                return render(response, IrParaInfo, {"user":sla})
+            if (logado.password == p):
+                logado.nome = n
+                logado.save()
+                return render(response, IrParaInfo, {"user":logado})
             else:
                 return JsonResponse(data = {"message": "Senha incorreta"})
     else:
         form = ChangeUserNameForm()
         return render(response, IrParaAlterar, {"form":form})
 
-def alterarLoginUser(response):
-    sla = User.objects.get(id = response.session['id_user'])
+def AlterarLoginUser(response):
+    logado = User.objects.get(id = response.session['id_user'])
     if response.method == "POST":
         form = ChangeUserEmailForm(response.POST)
 
         if form.is_valid():
             p = form.cleaned_data["password"]
             e = form.cleaned_data['email']
-            if (sla.password == p):
-                sla.email = e
-                sla.save()
-                return render(response, IrParaInfo, {"user":sla})
+            if (logado.password == p):
+                logado.email = e
+                logado.save()
+                return render(response, IrParaInfo, {"user":logado})
             else:
                 return JsonResponse(data = {"message": MensagemErro})
     else:
         form = ChangeUserEmailForm()
         return render(response, IrParaAlterar, {"form":form})
 
-def deletarUser(response):
-    sla = User.objects.get(id = response.session['id_user'])
+def DeletarMinhaConta(response):
+    logado = User.objects.get(id = response.session['id_user'])
     if response.method == "POST":
         form = ChangeUserEmailForm(response.POST)
 
         if form.is_valid():
             em = form.cleaned_data["email"]
             p = form.cleaned_data["password"]
-            if (sla.password == p) and (sla.email == em):
-                sla.delete()
+            if (logado.password == p) and (logado.email == em):
+                logado.delete()
                 form = LoginUserForm()
                 return render(response, IrParaLogin, {"form":form})
             else:
@@ -129,6 +129,7 @@ def deletarUser(response):
         return render(response, IrParaAlterar, {"form":form})
 
 def ListarUsuarios(response):
+    logado = User.objects.get(id = response.session['id_user'])
     if response.method == 'POST':
         if "pesquisar" in response.POST:
             lista = []
@@ -140,19 +141,28 @@ def ListarUsuarios(response):
                 lista.append(sla)
                 # Ele filtra pela pesquisa por nome
             return render(response, IrParaListarUsers, {'lista': lista})
-        for i in range(len(User.objects.exclude(id = response.session['id_user']))):
-            if str(i+1) in response.POST:
-                # i+1 é o id do usuario
-                visitar = User.objects.get(id=(i+1))
-
-                return render(response, IrParaVisita, {"user":visitar})
+        for i in User.objects.exclude(id = response.session['id_user']):
+            if str(i.id) in response.POST:
+                # Aqui ele verifica se o botão pressionado tem o id do user no select
+                visitar = i
+                response.session['id_visita'] = visitar.id
+                return render(response, IrParaVisita, {"user":logado, "visita":visitar})
+        return JsonResponse(data = {"message": MensagemErro})
     else:
-        
         lista = []
         for users in User.objects.exclude(id = response.session['id_user']):
             sla = []
             sla.append(users.nome)
             sla.append(users.id)
             lista.append(sla)
-            #lista recebe uma lista que possue sempre 2 valores, o primeiro é o nome do usuario e o segundo o seu id
+            #lista recebe uma lista que possue sempre 2 valores, o primeiro é o nome do usuario e o segundo o seu id, que ficará no nome do botão
         return render(response, IrParaListarUsers, {'lista': lista})
+
+def ApagarOutraConta(response):
+    logado = User.objects.get(id = response.session['id_user'])
+    ContaParaDeletar = User.objects.get(id = response.session['id_visita'])
+    if logado.permissionlevel >= 4:
+        ContaParaDeletar.delete()
+        return render(response, IrParaInfo, {"user":logado})
+    else:
+        return JsonResponse(data = {"message": MensagemErro})
