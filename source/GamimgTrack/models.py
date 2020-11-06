696d55dfa78a42ffa454f9c36f09cb3a02385a74
Geraldo Medeiros <geraldo_sjs@hotmail.com>
2020-11-05 22:48:53 -0300

    Melhoria nos models

--- source/GamimgTrack/models.py
+++ source/GamimgTrack/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
@@ -9,9 +11,18 @@ class User(models.Model):
     email = models.TextField()
     password = models.TextField()
     login = models.TextField()
-    creationdate = models.DateTimeField(db_column='creationDate', blank=True, null=True, auto_now_add=True)  # Field name made lowercase.
-    lastlogin = models.DateTimeField(db_column='lastLogin', blank=True, null=True)  # Field name made lowercase.
-    permissionlevel = models.IntegerField(db_column='permissionLevel', blank=True, null=True, default=1)  # Field name made lowercase.
+    creationdate = models.DateTimeField(db_column='creationDate', blank=True, null=True,
+                                        auto_now_add=True)  # Field name made lowercase.
+    lastlogin = models.DateTimeField(db_column='lastLogin', blank=True, null=True, auto_now_add=True)  # Field name made lowercase.
+    permissionlevel = models.IntegerField(db_column='permissionLevel', blank=True, null=True,
+                                          default=1)  # Field name made lowercase.
+
+    class Meta:
+        unique_together = ['email', 'login']
+
+    def __str__(self):
+        return self.nome
+
 
 """
 permissionlevel:
@@ -22,16 +33,23 @@ permissionlevel:
     5 = administrador
 """
 
+
 class Postagem(models.Model):
     id = models.AutoField(primary_key=True)
     likes = models.IntegerField(default=0)
     content = models.TextField()
     title = models.TextField()
     creation_date = models.DateTimeField(blank=True, null=True, auto_now_add=True)  # Field name made lowercase.
-    user_criador = models.ForeignKey(User, on_delete= models.SET_NULL, null=True)
+    user_criador = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
 
 class LikePostagens(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    Postagem = models.ForeignKey(Postagem, on_delete=models.CASCADE)
-    
+    user_like = models.ForeignKey(User, on_delete=models.CASCADE)
+    postagem_like = models.ForeignKey(Postagem, on_delete=models.CASCADE)
+
+    def __int__(self):
+        return self.user_like
