b5ac406ee01df2be43b7ec73f0397c7d33ee111a
pedrojonassm <pedrojonassm@gmail.com>
2020-11-21 11:28:12 -0300

    Mudado o manage.py para rodar o nose e o coverage ao rodar o manage.py test

--- source/manage.py
+++ source/manage.py
@@ -20,3 +20,19 @@ def main():
 
 if __name__ == '__main__':
     main()
+    from django.core.management import execute_from_command_line
+
+    is_testing = 'test' in sys.argv
+
+    if is_testing:
+        import coverage
+        cov = coverage.coverage(source=['package1', 'package2'], omit=['*/tests/*'])
+        cov.erase()
+        cov.start()
+
+    execute_from_command_line(sys.argv)
+
+    if is_testing:
+        cov.stop()
+        cov.save()
+        cov.report()
